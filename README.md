e43eedd473eb7dc0527b25d5ce7fa61876d7539d
Mete Ismayilzada <mismayilza@gmail.com>
2024-09-11 01:57:23 +0200

    add report gen docs

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Creative Story Generation Project
 
-# Setup
+## Setup
 Install dependencies
 ```sh
 pip install -r requirements.txt
@@ -10,3 +10,12 @@ Install spacy packages
 ```sh
 python -m spacy download en_core_web_sm
 ```
+
+## Generate reports
+First, define the report configuration under `experiments/reports/{experiment_name}/{report_name}` (see [`sample config.json`](experiments/reports/pilot/run1_report1/config.json)).
+Then you can use this config along with the experiment results to run `report_metrics.py` such as below:
+```sh
+python report_metrics.py -r "../experiments/outputs/gpt-4/pilot/**/run1" -o ../experiments/reports/{experiment_name}/${report_name}/gpt-4 -c ../experiments/reports/{experiment_name}/${report_name}/config.json
+```
+
+See examples in [report_metrics.sh](src/report_metrics.sh) and check out [report_metrics.py](src/report_metrics.py) for further details.
--- src/report_metrics.sh
+++ src/report_metrics.sh
@@ -3,7 +3,7 @@
 report_name=${1:-"run1_report1"}
 
 # pilot run 1
-# python report_metrics.py -r "../experiments/outputs/gpt-4/pilot/**/run1" -o ../experiments/reports/pilot/${report_name}/gpt-4 -c ../experiments/reports/pilot/${report_name}/config.json
-# python report_metrics.py -r "../experiments/outputs/gemini-1.5-flash/pilot/**/run1" -o ../experiments/reports/pilot/${report_name}/gemini-1.5-flash -c ../experiments/reports/pilot/${report_name}/config.json
+python report_metrics.py -r "../experiments/outputs/gpt-4/pilot/**/run1" -o ../experiments/reports/pilot/${report_name}/gpt-4 -c ../experiments/reports/pilot/${report_name}/config.json
+python report_metrics.py -r "../experiments/outputs/gemini-1.5-flash/pilot/**/run1" -o ../experiments/reports/pilot/${report_name}/gemini-1.5-flash -c ../experiments/reports/pilot/${report_name}/config.json
 python report_metrics.py -r "../experiments/outputs/claude-3-5-sonnet-20240620/pilot/**/run1" -o ../experiments/reports/pilot/${report_name}/claude-3-5-sonnet-20240620 -c ../experiments/reports/pilot/${report_name}/config.json
 python report_metrics.py -r "../experiments/outputs/human/pilot/run1" -o ../experiments/reports/pilot/${report_name}/human -c ../experiments/reports/pilot/${report_name}/config.json
