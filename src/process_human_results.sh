1c2ff9da1639d59b850c3cdfa30e438a62cac214
Mete Ismayilzada <mismayilza@gmail.com>
2024-12-12 22:24:41 +0100

    add preprocessed human results

---
+++ src/process_human_results.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python process_human_results.py -r ../experiments/outputs/human/pilot/CreativeStoryGeneration_20240910_1533.csv -c ../experiments/outputs/human/pilot/data_config.json -e run1
